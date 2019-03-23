96e199db7bb51e4680755c5c4a0fe926e0c0174f
Yushuo (Shawn) Han <37590422+shawnhan108@users.noreply.github.com>
2019-03-23 01:48:18 -0400

    Phase 1 and 2 completed for assembler.

--- Q7.rkt
+++ Q7.rkt
@@ -10,7 +10,7 @@
   (begin
     (hash-set! htable (car lst) ptr)
     (hash-set! htable ptr (cadr lst))
-    (setdata1-help htable (cddr lst) (+ 2 ptr))))
+    (setdata1-help htable (cddr lst) (+ 1 ptr))))
 
 ;function for data setup when data list in the form of '(NAME (occurences value)), returning the hashtable.
 (define (setdata2 htable lst ptr)
@@ -22,26 +22,72 @@
   (begin
     (hash-set! htable (car lst) ptr)
     (hash-set! htable ptr (second(cadr lst)))
-    (setdata2-help htable (list(sub1(car (second lst)))(cadr (second lst))) (+ 2 ptr))))
+    (setdata2-help htable (list (sub1 (car (second lst)))(cadr (second lst))) (+ 1 ptr))))
 
 (define (addinstruction htable lst ptr)
   (cond
     [(empty? lst) htable]
-    [(equal? (car (car lst)) 'const) (hash-set! htable (cadr (car lst)) (caddr (car lst)))
-                                     (addinstruction htable (cdr lst) (add1 ptr))]
-    [(equal? (car (car lst)) 'label) (hash-set! htable (cadr (car lst)) ptr)
-                                     (addinstruction htable (cdr lst) ptr)]
-    [(equal? (car (car lst)) 'data) (if (list? (caddr (car lst)))
+    [(equal? (car (car lst)) 'const) (if (hash-has-key? htable (second (car lst)))
+                                         (error "duplicate definition.")
+                                         (begin
+                                           (hash-set! htable (cadr (car lst)) (caddr (car lst)))
+                                           (addinstruction htable (cdr lst) ptr)))]
+    [(equal? (car (car lst)) 'label) (if (hash-has-key? htable (second (car lst)))
+                                         (error "duplicate definition.")
+                                         (begin
+                                           (hash-set! htable (cadr (car lst)) ptr)
+                                           (addinstruction htable (cdr lst) ptr)))]
+    [(equal? (car (car lst)) 'data) (if (hash-has-key? htable (second (car lst)))
+                                         (error "duplicate definition.")
+                                         (if (list? (caddr (car lst)))
                                         (addinstruction (setdata2 htable (cdr(car lst)) ptr)
                                                         (cdr lst)
-                                                        (+ 1 ptr (car (caddr(car lst)))))
+                                                        (+ ptr (car (caddr(car lst)))))
                                         (addinstruction (setdata1 htable (cdr (car lst)) ptr)
                                                         (cdr lst)
-                                                        (+ ptr (sub1 (length (car lst))))))]
+                                                        (+ ptr (- (length (car lst)) 2)))))]
     [(equal? (car (car lst)) 'halt) (hash-set! htable 0 0)
                                     (addinstruction htable (cdr lst) (add1 ptr))]))
+
+;returns a pair of (num (list of vars)) with all vars equal to the value of num, if circular then report error.
+(define (checkerror htable key)
+  (define (checkerror-h htable key myset)
+    (cond
+      [(symbol? (hash-ref htable key [(error "undefined usage")])) (if (set-member? myset (hash-ref htable key))
+                                           (error "circular data definition.")
+                                           (checkerror-h htable (hash-ref htable key) (set-add myset key)))]
+      [(number? (hash-ref htable key)) (list (hash-ref htable key) (set->list myset))]
+      ))
+  (if (symbol? (hash-ref htable key))
+      (checkerror-h htable key (set))
+      empty))
+
+(define (replace-val htable mylst) ;mylst in the form of (list num (list of vars))
+  (define (replace-help htable num varlst)
+    (cond
+      [(empty? varlst) htable]
+      [true (hash-set! htable (car varlst) num)
+       (replace-help htable num (cdr varlst))]))
+  (cond  
+    [(empty? mylst) htable]
+    [true (replace-help htable (car mylst) (second mylst))]))
+
+(define (check-replace htable)
+  (define table-lst (hash->list htable))
+  (define (cr-help htable hlst)
+    (cond
+      [(empty? hlst) htable]
+      [true (cr-help (replace-val htable (checkerror htable (car (car hlst))))
+                     (cdr hlst))]))
+  (cr-help htable table-lst))
 
 (define a '((label LABELONE) (label  LABELTWO) (label LABELTHREE)(const A B) (const B 3) (const C 5)
                              (data fish grouper bass tarpon tuna) (data people (5 names))))
 (define b '((data people (5 names))))
-(addinstruction storage a 0)
+(define c '((const A B)
+            (const B C)
+            ))
+(define d '((const A B) (const A C)(const B C) (const E F)(const C 2)(const F D)))
+
+(define htable (addinstruction storage c 0))
+
